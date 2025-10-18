6233d0b4b117ebc62c1f2ddfc6c97a31cc2cdd3f
mohanniranjan <niranjanma@elberttech.com>
2025-10-18 13:01:13 +0530

    free trail limit messages

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Provider> {children}</Provider>
+          <Provider> {children}<ToastContainer/></Provider>
         </body>
       </html>
     </ClerkProvider>
